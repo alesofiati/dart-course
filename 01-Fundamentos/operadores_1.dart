a7892c5edd8279b1ae565a9db1f92cb7c54ebf59
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2024-03-24 15:44:09 -0300

    Aula 1 de operadores

---
+++ 01-Fundamentos/operadores_1.dart
@@ -0,0 +1,22 @@
+main()
+{
+  // operadores aritimeticos
+  int a = 7;
+  int b = 3;
+
+  int resultado = a + b;
+  print(resultado);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+  print(a%b);
+
+  // operadores logicos
+  bool ehFragil = true;
+  bool ehCaro = false;
+
+  print(ehFragil && ehCaro);
+  print(ehFragil || ehCaro);
+  print(ehFragil ^ ehCaro);
+  print(!ehCaro);
+}
