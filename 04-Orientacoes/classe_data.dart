d142aaeb33745ae4df316cbe4a94520b7f7fc543
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2024-04-09 21:41:36 -0300

    Definindo construtor padrão para conter valores default

--- 04-Orientacoes/classe_data.dart
+++ 04-Orientacoes/classe_data.dart
@@ -1,15 +1,18 @@
 class Data {
-  int dia = 01;
-  int mes = 01;
-  int ano = 1970;
+  int dia;
+  int mes;
+  int ano;
   // Para defini um construtor nas classes em dart é necessário criar um método com o mesmo nome da classe
   // O construtor pode ser chamado de construtor padrão, pois ele é chamado quando a classe é instanciada
   // Caso não seja definidos parametros no contrutor, ele deixa de ser um construtor padrão.
-  Data(int diaInicial, int mesInicial, int anoInicial) {
-    dia = diaInicial;
-    mes = mesInicial;
-    ano = anoInicial;
-  }
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+  // Essa segunda opção é mais elegante e mais curta. Pois simplicia a declaração de atributos e a inicialização dos mesmos.
+  // O construtor padrão é chamado quando a classe é instanciada sem passar parametros.
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
 
   /**
    * Método que formata a data no formato dd/mm/aaaa
